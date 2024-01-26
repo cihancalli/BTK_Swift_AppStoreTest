3c41271309e0b7f77c07e704c5742391ad1bdb51
Cihan Çallı <cihan.callii@gmail.com>
2024-01-26 09:24:25 +0300

    Initial Commit

---
+++ AppStoreTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreTest
+//
+//  Created by Cihan on 26.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
